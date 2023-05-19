77cfd2259a31ed03afa25e4921eea881acc6df9b
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2023-05-19 17:29:39 -0300

    simple_factory

---
+++ creational/singleton/simple_factory_1.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC):
+    @abstractmethod
+    def buscar_cliente(self) -> None: pass
+
+class CarroLuxo(Veiculo):
+    def buscar_cliente(self) -> None:
+        print('Carro de luxo está buscando o cliente...')
+
+class CarroPopular(Veiculo):
+    def buscar_cliente(self) -> None:
+        print('Carro popular está buscando o cliente...')
